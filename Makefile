71cc223dbf14cf6e0eef1f7eb2bf622c56ae2625
aherlihy <aherlihy@cs.brown.edu>
2014-04-06 22:04:45 +0000

    finished cleaning up old files

--- Makefile
+++
@@ -1,7 +0,0 @@
-.PHONY: t
-
-all:
-	./py2llvm.py test.py
-
-t:
-	./py2llvm.py t/arith_test.py
--- run_bm.sh
+++
@@ -1,8 +0,0 @@
-echo "running bayes..."
-time bash llvm_bm.sh cpp_bayes.bc
-echo "running kmeans..."
-time bash llvm_bm.sh cpp_kmeans.bc
-echo "running linreg..."
-time bash llvm_bm.sh cpp_linreg.bc
-echo "running logreg..."
-time bash llvm_bm.sh cpp_logreg.bc
